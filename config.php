3a534cb1e171fef8d9bb93e6f2807ce5761dd916
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-04-18 16:30:46 +0900

    バリデーション実装

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DSN', 'mysql:host=db;dbname=task_app;charset=utf8');
+define('USER', 'testuser');
+define('PASSWORD', '9999');
+
+// エラーメッセージを定数として定義
+define('MSG_TITTLE_REQUIRED', 'タスク名を入力してください');
+
+// ステータスを定数として定義
+define('TASK_STATUS_NOTYET', 'notyet');
---
+++ functions.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once __DIR__ . '/config.php';
+
+function connectDb()
+{
+    try {
+        return new PDO(
+            DSN,
+            USER,
+            PASSWORD,
+            [PDO::ATTR_ERRMODE =>
+            PDO::ERRMODE_EXCEPTION]
+            );
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
+}
+
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+function insertValidate($title)
+{
+    $errors = [];
+
+    if ($title === '') {
+        $errors[] = MSG_TITTLE_REQUIRED;
+    }
+
+    return $errors;
+}
+function insertTask($title)
+{
+    try {
+        $dbh = connectDb();
+        $sql = <<<EOM
+        INSERT INTO
+            tasks
+            (title)
+        VALUES
+            (:title);
+        EOM;
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+function createErrMsg($errors)
+{
+    $err_msg = "<ul class=\"errors\">\n";
+
+    foreach ($errors as $error) {
+        $err_msg .= "<li>" . h($error) . "</li>\n";
+    }
+
+    $err_msg .= "</ul>\n";
+
+    return $err_msg;
+}
+
+function findTaskByStatus($status)
+{
+    $dbh = connectDb();
+
+    $sql = <<<EOM
+    SELECT
+        *
+    FROM
+        tasks
+    WHERE
+        status = :status;
+    EOM;
+
+    // プリペアドステートメントの準備
+    $stmt = $dbh->prepare($sql);
+
+    // バインドするパラメータの準備
+    $status = 'notyet';
+
+    // パラメータのバインド
+    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
+
+    // プリペアドステートメントの実行
+    $stmt->execute();
+
+    // 結果の取得
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
