43111b5343e715db9852dad58a3d820b89f2473d
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-04-18 16:56:17 +0900

    完了処理まで終わり

--- functions.php
+++ functions.php
@@ -63,6 +63,21 @@ function createErrMsg($errors)
 
     return $err_msg;
 }
+function updateStatusToDone($id)
+{
+    $dbh = connectDb();
+    $sql = <<<EOM
+    UPDATE
+        tasks
+    SET
+        status = 'done'
+    WHERE
+        id = :id
+    EOM;
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+}
 
 function findTaskByStatus($status)
 {
@@ -79,9 +94,6 @@ function findTaskByStatus($status)
 
     // プリペアドステートメントの準備
     $stmt = $dbh->prepare($sql);
-
-    // バインドするパラメータの準備
-    $status = 'notyet';
 
     // パラメータのバインド
     $stmt->bindParam(':status', $status, PDO::PARAM_STR);
--- index.php
+++ index.php
@@ -14,7 +14,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         insertTask($title);
     }
 }
+
+// 未完了タスクの取得
 $notyet_tasks = findTaskByStatus(TASK_STATUS_NOTYET);
+
+// 完了タスクの取得
+$done_tasks = findTaskByStatus(TASK_STATUS_DONE);
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -35,7 +41,7 @@ $notyet_tasks = findTaskByStatus(TASK_STATUS_NOTYET);
             <ul>
                 <?php foreach ($notyet_tasks as $task) : ?>
                     <li>
-                        <a href="" class="btn done-btn">完了</a>
+                        <a href="done.php?id=<?= h($task['id']) ?>" class="btn done-btn">完了</a>
                         <a href="" class="btn edit-btn">編集</a>
                         <a href="" class="btn delete-btn">削除</a>
                         <?= h($task['title']) ?>
@@ -45,9 +51,11 @@ $notyet_tasks = findTaskByStatus(TASK_STATUS_NOTYET);
         </div>
         <div class="done-task">
             <h2>完了タスク</h2>
-            <ul>
-                <li>完了テストタスク</li>
-            </ul>
+            <?php foreach ($done_tasks as $task) : ?>
+                <ul>
+                    <li><?= $task['title'] ?></li>
+                </ul>
+            <?php endforeach; ?>
         </div>
     </div>
 </body>
